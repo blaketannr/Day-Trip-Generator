65ba38a19c065bc30886f205f13b685d6f393efe
blaketannr <btanner980@gmail.com>
2021-01-20 13:55:16 -0600

    I have created individual functions that are used to break up and operate different parts of the code. I added parts of the code into different functions to make the perform smoother and makes the overall day trip generator easier to maintain.

--- trip.js
+++ trip.js
@@ -43,39 +43,34 @@
 //  else if (userInput === "no"){
 //     alert("Nope, try again!");
 //  }
-
 // }
-
-
-let userInput = "no";
-while(userInput === "no"){
- 
 let destinations = ['Madison', 'California', 'Kansas', 'New York'];
 let restaurants = ['Mcdonalds', 'Olive Garden', 'Burger King', 'Wendys'];
 let transportations = ['Bus', 'Car', 'Train', 'Plane'];
 let entertainments = ['Lasertag', 'Arcade', 'Brewery Tour', 'Movies'];
 
-alert("heres your trip itinerary");
+let userInput = "no";
+while(userInput === "no"){
+userInput = prompt("Do you like this trip?");
+
+
 let sumNumber = Math.floor(Math.random() * 4);
-
-function displayTrip(){
-
-alert("your destination will be "  +  destinations[sumNumber] + '');
-alert("your restuarant will be " + restaurants[sumNumber] + '');
-alert("your transportation will be "  + transportations[sumNumber] + '');
-alert("your entertainment will be " + entertainments[sumNumber] + '');
-
-console.log(displayTrip);
-
-}
-
-userInput = prompt("do you like this trip?");
-
+console.log(sumNumber);
+function tripOptions() {
 if (userInput === "yes"){
     alert("great, have fun!");
 }
  else if (userInput === "no"){
     alert("Nope, try again!");
  }
+}
+tripOptions();
+function displayTrip() {
 
+    console.log("your destination will be "  +  destinations[sumNumber] + '');
+    console.log("your restuarant will be " + restaurants[sumNumber] + '');
+    console.log("your transportation will be "  + transportations[sumNumber] + '');
+    console.log("your entertainment will be " + entertainments[sumNumber] + '');
+    console.log("your planned day trip will be " + myTrip[sumNumber] + '' );
+}
 }
